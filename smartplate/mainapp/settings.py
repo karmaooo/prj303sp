b1eec22ec1b4f4aa5e5d01b4047825ed053d9739
KARMA YANGZOM <12200052.gcit@rub.edu.bt>
2023-06-08 19:52:44 +0600

    commit

--- smartplate/mainapp/settings.py
+++ smartplate/mainapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ztgworv%36wj^blkq*=z9r733ihbgh_ln&qp0ah9%apb9d+smh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 CSRF_USE_SESSIONS = True
 
 ALLOWED_HOSTS = ["*"]
@@ -124,12 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/" )
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/"),
     ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build","static" )
-
 # STATICFILES_FINDERS = [
 #     'django.contrib.staticfiles.finders.FileSystemFinder',
 #     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
