77977aee706ff1f94c25ffee0cde3522f0957caf
KARMA YANGZOM <12200052.gcit@rub.edu.bt>
2023-06-08 20:12:38 +0600

    c

--- smartplate/mainapp/local_settings.py
+++ smartplate/mainapp/local_settings.py
@@ -6,14 +6,25 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+import os
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "smartPlate",
-        "USER": "postgres",
-        "PASSWORD": "connect",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv("RAILWAY_POSTGRES_DB"),
+        "USER": os.getenv("RAILWAY_POSTGRES_USER"),
+        "PASSWORD": os.getenv("RAILWAY_POSTGRES_PASSWORD"),
+        "HOST": os.getenv("RAILWAY_POSTGRES_HOST"),
+        "PORT": os.getenv("RAILWAY_POSTGRES_PORT"),
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "smartPlate",
+#         "USER": "postgres",
+#         "PASSWORD": "connect",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     } 
+# }
